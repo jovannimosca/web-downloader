41c315af9ba435a5bfc6df74de2b5a852534b96a
Jovanni Mosca <jovanni.mosca@proton.me>
2025-02-23 21:19:13 -0500

    feat(scripts): creates install and start scripts for linux server

---
+++ scripts/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pkg_dir=$(git rev-parse --show-toplevel)
+backend_dir="${pkg_dir}/backend"
+frontend_dir="${pkg_dir}/frontend"
+
+# Install application-wide dependencies
+echo "Installing application-wide dependencies..."
+sudo apt-get update -y
+sudo apt-get install -y python3 python3-pip python3-venv nodejs npm apache2
+
+# Install backend dependencies
+echo "Installing backend dependencies..."
+cd $backend_dir
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+# Install frontend dependencies
+echo "Installing frontend dependencies..."
+cd $frontend_dir
+npm install
+npm run build
+cp -r dist/* /var/www/html
---
+++ scripts/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pkg_dir=$(git rev-parse --show-toplevel)
+backend_dir="${pkg_dir}/backend"
+frontend_dir="${pkg_dir}/frontend"
+
+# Start the backend
+echo "Starting the backend..."
+cd $backend_dir
+source .venv/bin/activate
+uvicorn app:app --host 0.0.0.0 --port 8080 --workers 4
+
+# Start the frontend
+echo "Starting the frontend..."
+cd $frontend_dir
+sudo systemctl start apache2
