69b21004ef60f714954548947cde72df8bf2aa72
Jovanni Mosca <jovanni.mosca@proton.me>
2025-02-23 22:33:06 -0500

    fix(start-script): start frontend first because it is non-blocking

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,13 +5,13 @@ pkg_dir=$(git rev-parse --show-toplevel)
 backend_dir="${pkg_dir}/backend"
 frontend_dir="${pkg_dir}/frontend"
 
+# Start the frontend
+echo "Starting the frontend..."
+cd $frontend_dir
+systemctl start apache2
+
 # Start the backend
 echo "Starting the backend..."
 cd $backend_dir
 source .venv/bin/activate
 uvicorn app:app --host 0.0.0.0 --port 8080 --workers 4 > $backend_dir/backend.log 2>&1
-
-# Start the frontend
-echo "Starting the frontend..."
-cd $frontend_dir
-systemctl start apache2
