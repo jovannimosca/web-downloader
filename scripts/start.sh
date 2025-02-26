1b00fad8bb437c8bec3a2cbe5b1fe02442c0e2a1
Jovanni Mosca <jovanni.mosca@proton.me>
2025-02-25 20:25:43 -0500

    feat(pid): added PID management to allow for stopping

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+PID_FILE = "/tmp/web-downloader.pid"
 pkg_dir=$(git rev-parse --show-toplevel)
 backend_dir="${pkg_dir}/backend"
 frontend_dir="${pkg_dir}/frontend"
@@ -14,4 +15,7 @@ systemctl start apache2
 echo "Starting the backend..."
 cd $backend_dir
 source .venv/bin/activate
-uvicorn app:app --host 0.0.0.0 --port 8080 --workers 4 > $backend_dir/backend.log 2>&1
+uvicorn app:app --host 0.0.0.0 --port 8080 --workers 4 > $backend_dir/backend.log 2>&1 &
+
+echo $! > $PID_FILE
+echo "Web downloader started with PID: $(cat $PID_FILE)"
